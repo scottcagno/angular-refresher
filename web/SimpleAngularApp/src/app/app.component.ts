d6dcdb6e180c9469fac84b6de68b11afe85993b7
Scott Cagno <scottiecagno@gmail.com>
2022-10-11 16:32:11 -0400

    [SimpleAngularApp] -- generated header, page1, page2, page3 and footer components

--- web/SimpleAngularApp/src/app/app.component.ts
+++ web/SimpleAngularApp/src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SimpleAngularApp';
+
+  @ViewChild('footer', {static: true})
+  footerComponent!: FooterComponent;
+
+  startTime!: string;
+
+  updateLastAccessed() {
+    this.footerComponent.lastAccessed = new Date().toString();
+  }
+
+  ngOnInit() {
+    this.startTime = new Date().toString();
+  }
 }
