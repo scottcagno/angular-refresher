d97d0f4af70667c1b43bcdfb319c3630b8386d70
Scott Cagno <scottiecagno@gmail.com>
2022-10-11 15:06:34 -0400

    [SimpleAngularApp] -- generated header, page1, page2, page3 and footer components

--- web/SimpleAngularApp/src/app/app.module.ts
+++ web/SimpleAngularApp/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { HeaderComponent } from './header/header.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    HeaderComponent,
+    Page2Component,
+    Page3Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
