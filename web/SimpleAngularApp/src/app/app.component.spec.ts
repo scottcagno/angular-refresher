83d0b5ae549e6a16565335ed1e9258ff27a56993
Scott Cagno <scottiecagno@gmail.com>
2022-10-13 15:20:16 -0400

    [ExploringTypescript] -- module 2, chapter 7

--- web/SimpleAngularApp/src/app/app.component.spec.ts
+++ web/SimpleAngularApp/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {Page1Component} from "./page1/page1.component";
+import {Page2Component} from "./page2/page2.component";
+import {Page3Component} from "./page3/page3.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        Page1Component,
+        Page2Component,
+        Page3Component,
+        FooterComponent,
       ],
     }).compileComponents();
   });
--- web/SimpleAngularApp/src/app/data.service.spec.ts
+++ web/SimpleAngularApp/src/app/data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import {Book} from "./model/book";
 
 describe('DataService', () => {
   let service: DataService;
@@ -12,5 +13,12 @@ describe('DataService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('addBook increases the size of the books array', ()=>{
+    const book = new Book();
+    service.addBook(book);
+    //fail(); // to fail a test
+    expect(service.books.length).toEqual(4);
   });
 });
