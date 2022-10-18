52137df9d1758e0e0375f384f41789bcb91002e0
Scott Cagno <scottiecagno@gmail.com>
2022-10-18 15:43:10 -0400

    [RoomBookingClient] -- module 3, chapter 7

--- web/RoomBookingClient/src/app/admin/rooms/rooms.component.ts
+++ web/RoomBookingClient/src/app/admin/rooms/rooms.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DataService} from "../../data.service";
 import {Room} from "../../model/Room";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit, OnDestroy {
 
   rooms !:Array<Room>;
+  selectedRoom !:Room;
 
-  constructor(private dataService :DataService) {
+  constructor(private dataService :DataService, private route :ActivatedRoute, private router :Router) {
 
   }
 
   ngOnInit(): void {
     this.rooms = this.dataService.rooms;
+    this.route.queryParams.subscribe(
+      (params) => {
+        const id = params['id'];
+        if (id) {
+          // @ts-ignore
+          this.selectedRoom = this.rooms.find((room)=> {
+            // + symbol before a string, converts a string into a number
+            return room.id === +id
+          });
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+  }
+
+  selectRoom(id :number) {
+    this.router.navigate(['admin','rooms'], {queryParams:{id:id}})
   }
 
 }
