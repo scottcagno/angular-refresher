a61dfeae513bbf73f3359ad32a3d08ea96181c88
Scott Cagno <scottiecagno@gmail.com>
2022-10-21 16:29:58 -0400

    [RoomBookingClient] -- m3c7

--- web/RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
+++ web/RoomBookingClient/src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Room} from "../../../model/Room";
+import {Layout, LayoutCapacity, Room} from "../../../model/Room";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-room-edit',
@@ -9,15 +10,50 @@ import {Room} from "../../../model/Room";
 export class RoomEditComponent implements OnInit {
 
   @Input()
-  room !:Room;
+  room!: Room;
 
-  constructor() { }
+  layouts = Object.keys(Layout);
+  layoutEnum = Object(Layout);
+
+  roomForm = new FormGroup(
+    {
+      roomName : new FormControl('name'),
+      location : new FormControl('location'),
+    }
+  );
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.roomForm.patchValue({
+      roomName:this.room.name,
+      location: this.room.location,
+    });
+    for (const layout of this.layouts) {
+      // @ts-ignore
+      this.roomForm.addControl(`layout${layout}`, new FormControl(`layout${layout}`));
+    }
   }
 
   onCancel() {}
 
-  onSubmit() {}
+  onSubmit() {
+    this.room.name = <string>this.roomForm.controls['roomName'].value;
+    this.room.location = <string>this.roomForm.controls['location'].value;
+    this.room.capacities = new Array<LayoutCapacity>();
+    for (const layout of this.layouts) {
+      const layoutCapacity = new LayoutCapacity();
+      // @ts-ignore
+      layoutCapacity.layout = Layout[layout];
+      // @ts-ignore
+      layoutCapacity.capacity = <number>this.roomForm.controls[`layout${layout}`].value;
+      this.room.capacities.push(layoutCapacity);
+    }
+    console.log(this.room);
+  }
+
+  toTitle(s :string) :string {
+      return s.split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' ');
+  }
 
 }
--- web/RoomBookingClient/src/app/model/Room.ts
+++ web/RoomBookingClient/src/app/model/Room.ts
@@ -1,11 +1,12 @@
+
 export class Room {
 
-  id !:number;
-  name !:string;
-  location !:string;
-  capacities  !:Array<LayoutCapacity>;
+  id!: number;
+  name!: string;
+  location!: string;
+  capacities!: Array<LayoutCapacity>;
 
-  constructor(id ?:number, name ?:string, location ?:string) {
+  constructor(id?: number, name?: string, location?: string) {
     if (id) { this.id = id }
     if (name) { this.name = name }
     if (location) { this.location = location }
@@ -15,10 +16,10 @@ export class Room {
 }
 
 export class LayoutCapacity {
-  layout !:Layout;
-  capacity !:number;
+  layout!: Layout;
+  capacity!: number;
 
-  constructor(layout ?: Layout, capacity ?: number) {
+  constructor(layout?: Layout, capacity?: number) {
     if (layout) { this.layout = layout }
     if (capacity) { this.capacity = capacity }
   }
@@ -29,3 +30,38 @@ export enum Layout {
   USHAPE = 'U-Shape',
   BOARD = 'Board Meeting'
 }
+
+// export namespace Layout {
+//   export function value(layout: string): typeof Layout[keyof typeof Layout] {
+//     const val = Object.keys(Layout).find(key => Layout[key as keyof typeof Layout] == layout);
+//     return val as typeof Layout[keyof typeof Layout];
+//   }
+// }
+
+/*
+enum LayoutOptions {
+  THEATER = 'Theater',
+  USHAPE = 'U-Shape',
+  BOARD = 'Board Meeting',
+}
+
+export type LayoutOptionKey = keyof typeof LayoutOptions
+export type LayoutOptionValue = typeof LayoutOptions[keyof typeof LayoutOptions];
+export type LayoutOption = [LayoutOptionKey, LayoutOptionValue];
+
+export const Layouts = new Map<LayoutOptionKey,LayoutOptionValue>([
+    LayoutOption(LayoutOptions, LayoutOptions.THEATER),
+    LayoutOption(LayoutOptions, LayoutOptions.USHAPE),
+    LayoutOption(LayoutOptions, LayoutOptions.BOARD),
+  ]
+);
+
+function LayoutKey(obj: typeof Layouts, value: LayoutOptionValue) :LayoutOptionKey {
+  return Object.keys(obj).find(key => obj[key as keyof typeof Layouts] === value) as LayoutOptionKey;
+}
+
+function LayoutOption(options: typeof LayoutOptions, value: LayoutOptionValue) :LayoutOption {
+  return [Object.keys(options).find(key => options[key as keyof typeof options] === value) as LayoutOptionKey, value];
+}
+ */
+
